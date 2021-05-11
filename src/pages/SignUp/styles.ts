906808bdf15d69f37d6562848ab60f7cf4490666
Eduardo Torres <eduutorres@hotmail.com>
2021-05-11 19:26:00 -0300

    Tela cadastro

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImg from '../../assets/sign-in-background.png';
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -17,8 +17,8 @@ export const Content = styled.div`
 
   place-content: center;
 
-  width: 100%;
-  max-width: 700px;
+  width: 90%;
+  max-width: 600px;
 
   form {
     margin: 50px 0;
@@ -43,7 +43,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #ff9000;
+    color: #f4ede8;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -57,13 +57,13 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#f4ede8')};
     }
   }
 `;
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signInBackgroundImg}) no-repeat center;
+  background: url(${signUpBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
