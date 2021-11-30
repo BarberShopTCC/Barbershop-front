60a5d28b6de21dc460f7e738ed14816a87513111
Luiz Eduardo <eduutorres@hotmail.com>
2021-11-30 16:49:31 -0300

    api backurl

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'https://teste-backedu.herokuapp.com/',
+  baseURL: 'https://backend-barbershop.herokuapp.com/',
 });
 
 export default api;
